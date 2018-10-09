e6c992beeab1a8a4b36c6bf99a0a5421466e0307
MISHRA, ADITYA (apmishra) <am746c@att.com>
2018-10-08 17:27:58 -0700

    Data Processing Pipeline for Streaming DataSets
    Added README to All python files
    
    Issue-ID: ACUMOS-1198
    
    Change-Id: I088de46d86277f7d35f62673a8fa4614355d9b27
    Signed-off-by: MISHRA, ADITYA (apmishra) <am746c@att.com>

---
+++ DataPipeline/swagger_server/models/__init__.py
@@ -0,0 +1,40 @@
+#ACUMOS
+#================================================================================
+#Copyright Â© 2018 AT&T Intellectual Property. All rights reserved.
+#================================================================================
+#This Acumos software file is distributed by AT&Tunder the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#This file is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and 
+#limitations under the License.
+
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.data_pipeline_service_data_source import DataPipelineServiceDataSource
+from swagger_server.models.data_pipeline_service_data_source_pool import DataPipelineServiceDataSourcePool
+from swagger_server.models.data_pipeline_service_empty import DataPipelineServiceEmpty
+from swagger_server.models.data_pipeline_service_extract import DataPipelineServiceExtract
+from swagger_server.models.data_pipeline_service_id_metrics import DataPipelineServiceIdMetrics
+from swagger_server.models.data_pipeline_service_identifier import DataPipelineServiceIdentifier
+from swagger_server.models.data_pipeline_service_identifier_type import DataPipelineServiceIdentifierType
+from swagger_server.models.data_pipeline_service_kv_pair import DataPipelineServiceKVPair
+from swagger_server.models.data_pipeline_service_kv_pairs import DataPipelineServiceKVPairs
+from swagger_server.models.data_pipeline_service_objects import DataPipelineServiceObjects
+from swagger_server.models.data_pipeline_service_persistencetype import DataPipelineServicePERSISTENCETYPE
+from swagger_server.models.data_pipeline_service_pools import DataPipelineServicePools
+from swagger_server.models.data_pipeline_service_processor_payload import DataPipelineServiceProcessorPayload
+from swagger_server.models.data_pipeline_service_processor_topic import DataPipelineServiceProcessorTopic
+from swagger_server.models.data_pipeline_service_processor_type import DataPipelineServiceProcessorType
+from swagger_server.models.data_pipeline_service_servicestatus import DataPipelineServiceSERVICESTATUS
+from swagger_server.models.data_pipeline_service_state import DataPipelineServiceState
+from swagger_server.models.data_pipeline_service_topic import DataPipelineServiceTopic
+from swagger_server.models.data_pipeline_service_topic_data_source import DataPipelineServiceTopicDataSource
+from swagger_server.models.data_pipeline_service_topic_pipeline import DataPipelineServiceTopicPipeline
